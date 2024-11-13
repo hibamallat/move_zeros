a3cbe99d1dc4fa656eeea9e69efb07c3d4bf1f6f
hibamallat <hibamallat040@gmail.com>
2024-11-13 15:30:33 +0200

    Add a few comment lines

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ of the list while maintaining the order of the other elements.
 """
 
 def move_zeros(numbers):
+    # Create two separate lists: one for non-zero numbers and one for zeros
     zeros_list= []
     non_zeros_list= []
 
@@ -14,16 +15,19 @@ def move_zeros(numbers):
         else:
             non_zeros_list.append(numbers[i])
 
+    # Combine the non-zero list and the zeros list (zeros at the end)
     return non_zeros_list + zeros_list
 
 list_nums = []
 try:
-    size = int(input("How many numbers you would to put inthe list? "))
+    size = int(input("How many numbers would you like to add to the list? "))
+
     for i in range (size):
+        # Collect user numbers and add them to list_nums
         user_num = int(input(f"Please enter a number {i + 1}: "))
         list_nums.append(user_num)
 
-    print("The original list:",list_nums)
+    print(f"The original list: {list_nums}")
     move_zeros_list = move_zeros(list_nums)
     print(f"After moving zeros list: {move_zeros_list}")
 
